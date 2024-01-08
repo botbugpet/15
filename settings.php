adb8076b6d5461796b6ac7612e631e9e9a8aa72e
botbugpet <125388796+botbugpet@users.noreply.github.com>
2024-01-08 12:14:17 +0700

    Add files via upload

---
+++ settings.php
@@ -0,0 +1,11 @@
+<?php
+
+$useragent = "Mozilla/5.0 (Linux; Android 10; RMX2030) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.103 Mobile Safari/537.36";
+
+
+$cookie = "ci_session=i6frovisp93p7tuk8at3jjtfcs557442; Referral_Source=https://search.yahoo.com/; csrf_cookie_name=e7d3ec7d0519038ecade5c1f03cdfbd8";
+
+
+$apikey = "yjoXR9QKRjZcoseAcW3rzr61dgydgabJ";
+
+
